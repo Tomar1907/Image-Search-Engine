00dcda7ba3079548483393c8057fed8ab6b40fef
Tomar1907 <soumyatomar1908@gmail.com>
2025-04-20 10:54:14 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const searchForm = document.getElementById("search-form")
-const searchBox = document.getElementById("search-box")
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result")
 const showMoreBtn = document.getElementById("show-more-btn")
 
