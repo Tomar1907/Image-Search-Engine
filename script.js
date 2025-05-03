4394636fd46147cd19140ece5616a280d22f599b
Soumya Tomar <soumyatomar1908@gmail.com>
2025-05-03 14:33:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const showMoreBtn = document.getElementById("show-more-btn");
 let keyword = "";
 let page = 1;
 
+
 async function searchImages(){
     keyword = searchBox.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${UNSPLASH_KEY}&per_page=12`;
